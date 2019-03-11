b8aa11b8ad9286fd5a33e24b6534417655f95d4f
DiWorm <dyworm@gmail.com>
2019-03-11 13:59:33 +0300

    homework-04

--- homework-03.py
+++ homework-03.py
@@ -6,5 +6,16 @@ wd.get('http://litecart.stqa.ru/admin/')
 wd.find_element_by_name('username').send_keys('admin')
 wd.find_element_by_name('password').send_keys('0b7dba1c77df25bf0')
 wd.find_element_by_name('login').click()
+
+
 wd.quit()
 sys.exit()
+
+
+
+
+
+
+
+
+
---
+++ homework-04.py
@@ -0,0 +1,18 @@
+import sys
+from selenium import webdriver
+
+wd = webdriver.Chrome()
+wd.get('http://litecart.stqa.ru/admin/')
+wd.find_element_by_name('username').send_keys('admin')
+wd.find_element_by_name('password').send_keys('0b7dba1c77df25bf0')
+wd.find_element_by_name('login').click()
+wd.quit()
+
+wd = webdriver.Firefox()
+wd.get('http://litecart.stqa.ru/admin/')
+wd.find_element_by_name('username').send_keys('admin')
+wd.find_element_by_name('password').send_keys('0b7dba1c77df25bf0')
+wd.find_element_by_name('login').click()
+wd.quit()
+sys.exit()
+
