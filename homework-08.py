9886f2804cc1ea6e9d43cb5bb2be7e49a487f5f0
DiWorm <dyworm@gmail.com>
2019-03-12 17:04:13 +0300

    homework-08

--- homework-08.py
+++ homework-08.py
@@ -1,5 +1,3 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -8,12 +6,16 @@ wait = WebDriverWait(wd, 10) # seconds
 wd.get("http://litecart.stqa.ru")
 
 root_elements = wd.find_elements_by_css_selector("div.content a.link")
-
-
-ducks = []
+i = 0
 for root_element in root_elements:
-    ducks.append(root_element.text)
+    have_sticker = root_element.find_elements_by_css_selector("div.sticker")
+    if len(have_sticker) == 0:
+        print(root_element.get_attribute("title"), "\t no sticker")
+    elif len(have_sticker) == 1:
+        print(root_element.get_attribute("title"), "\t 1 sticker")
+    else:
+        print(root_element.get_attribute("title"), "\t more then 1 sticker")
+wd.quit()
 
-print(ducks)
 
 
