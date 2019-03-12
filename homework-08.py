651cad3e42eba100d5aecdbf326636950eb37185
DiWorm <dyworm@gmail.com>
2019-03-12 17:13:16 +0300

    homework-08

--- homework-08.py
+++ homework-08.py
@@ -9,12 +9,17 @@ root_elements = wd.find_elements_by_css_selector("div.content a.link")
 
 for root_element in root_elements:
     have_sticker = root_element.find_elements_by_css_selector("div.sticker")
+
+    ''' test
     if len(have_sticker) == 0:
         print(root_element.get_attribute("title"), "\t no sticker")
     elif len(have_sticker) == 1:
         print(root_element.get_attribute("title"), "\t 1 sticker")
     else:
         print(root_element.get_attribute("title"), "\t more then 1 sticker")
+    '''
+    if len(have_sticker) == 1:
+        print(root_element.get_attribute("title"), "\t 1 sticker")
 wd.quit()
 
 
