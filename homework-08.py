02df12d45dde392cf485d7c5dce3196fb95d6f10
DiWorm <dyworm@gmail.com>
2019-03-12 17:05:22 +0300

    homework-08

--- homework-08.py
+++ homework-08.py
@@ -6,7 +6,7 @@ wait = WebDriverWait(wd, 10) # seconds
 wd.get("http://litecart.stqa.ru")
 
 root_elements = wd.find_elements_by_css_selector("div.content a.link")
-i = 0
+
 for root_element in root_elements:
     have_sticker = root_element.find_elements_by_css_selector("div.sticker")
     if len(have_sticker) == 0:
