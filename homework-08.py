6d36db571374142062e326db2135ba14f54d1134
DiWorm <dyworm@gmail.com>
2019-03-13 09:19:24 +0300

    homework-08 fix

--- homework-08.py
+++ homework-08.py
@@ -1,3 +1,7 @@
+'''
+если у всех товаров стикеры есть -- сценарий должен завершиться успешно. если у какого-то товара стикера нет -- сценарий должен падать. выводить что-то на консоль или не выводить -- это уже ваше дело
+'''
+
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -5,10 +9,14 @@ wd = webdriver.Chrome()
 wait = WebDriverWait(wd, 10) # seconds
 wd.get("http://litecart.stqa.ru")
 
-root_elements = wd.find_elements_by_css_selector("div.content a.link")
+root_elements = wd.find_elements_by_css_selector("li.product > a")
 
+i = 0
 for root_element in root_elements:
     have_sticker = root_element.find_elements_by_css_selector("div.sticker")
+    if len(have_sticker) == 1:
+        i += 1
+
 
     ''' test
     if len(have_sticker) == 0:
@@ -18,8 +26,16 @@ for root_element in root_elements:
     else:
         print(root_element.get_attribute("title"), "\t more then 1 sticker")
     '''
+    ''' 
     if len(have_sticker) == 1:
         print(root_element.get_attribute("title"), "\t 1 sticker")
+    '''
+
+if len(root_elements) != i:
+    print("Не у всех товаров есть стикер")
+else:
+    print("У всех товаров есть стикер")
+
 wd.quit()
 
 
