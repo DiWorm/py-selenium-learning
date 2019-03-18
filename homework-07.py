10d08025233edd8daa3f0f1bb04d0fcc50531fba
DiWorm <dyworm@gmail.com>
2019-03-18 16:43:02 +0300

    homework-09

--- homework-07.py
+++ homework-07.py
@@ -1,3 +1,10 @@
+'''
+Сделайте сценарий, который выполняет следующие действия в учебном приложении litecart.
+
+1) входит в панель администратора http://localhost/litecart/admin
+2) прокликивает последовательно все пункты меню слева, включая вложенные пункты
+3) для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
+'''
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
